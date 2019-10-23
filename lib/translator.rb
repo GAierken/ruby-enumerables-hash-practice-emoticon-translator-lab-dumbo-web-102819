c18763fd104797f729ecd80f13741e3a0cf50303
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 14:56:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,8 @@ def get_japanese_emoticon(file_path,emoticon)
   if result
     result
   else
-    puts "Sorry, that emoticon was not found"
-  end 
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
