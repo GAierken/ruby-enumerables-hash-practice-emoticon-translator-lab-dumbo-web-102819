1209dd5e446775a85f860678bed67da9177e2dd6
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 14:14:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require "yaml"# require modules here
 
 def load_library (yaml)
-  library= YAML.load_file ("./lib/emoticons.yml")
+  emoticons= YAML.load_file ("./lib/emoticons.yml")
   library
   # code goes here
 end
